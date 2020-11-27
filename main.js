baa40d3eb25a72ccd394238f71cca9d82932ffe9
Wojo3214 <55576529+Wojo3214@users.noreply.github.com>
2020-11-27 20:49:57 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 "use strict"
+//global array
 let _cars = [];
 
 //Getting data from JSON file
 async function loadData(){
-    let data = await fetch ('http://duckwise.net/assignments/frontend-internship/cars.json').then(res => res.json());
-    console.log(data);
-    _cars = data.data;
+    let jsonData = await fetch ('https://duckwise.net/assignments/frontend-internship/cars.json',{
+        mode: "no-cors"
+    }).then(res => res.json()).catch(error => console.log(error));
+    console.log(jsonData);
+    _cars = jsonData.data;
 
     appendCars(_cars);
 }
@@ -16,22 +19,9 @@ loadData();
 function appendCars(cars){
     let htmlTemplate = "";
     for (let car of cars) {
-        if(!car.images){
-            htmlTemplate +=`
-    
-        <article class="car-content">
-            <div class="main-info">
-                <h2 class="car-brand">${car.initials.brand}</h2>
-                <p class="car-model">${car.title}</p>
-                <p class="car-price"><i class="fas fa-dollar-sign"></i> ${car.initials.price} DKK</p>
-
-                <a href="#${car.initials.brand}/${car.title}" onclick="appendDetails(${car.ID})" class="more">More details</a>
-            </div>
-        </article>`
-        } else {
         htmlTemplate +=`
         <article class="car-content">
-            <a href="#${car.initials.brand}/${car.title}" onclick="appendDetails(${car.ID})"><img src="${car.images[0]}" alt="${car.title}"></a>
+            <a href="#${car.initials.brand}/${car.title}" onclick="appendDetails(${car.ID})">${getImageSource(car, car.title)}</a>
             <div class="main-info">
                 <h2 class="car-brand">${car.initials.brand}</h2>
                 <p class="car-model">${car.title}</p>
@@ -40,12 +30,14 @@ function appendCars(cars){
                 <a href="#${car.initials.brand}/${car.title}" onclick="appendDetails(${car.ID})" class="more">More details</a>
             </div>
         </article>`
-        }
     }
     document.querySelector(".container").innerHTML += htmlTemplate;
 }
 
-//Display details info
+//Show first image or nothing
+let getImageSource = (car, label) => (car.images) ? `<img src="${car.images[0]}" alt="${label}">` : '' ;
+
+//Display details info about clicked car
 function appendDetails(carId){
     console.log(carId);
     
@@ -56,64 +48,52 @@ function appendDetails(carId){
         }
     }
     console.log(specificCarDetails.title);
-    
+    let mainFeatureData = (exactData, label) => (exactData.data) ? label + exactData.data + exactData.after : '' ;
+    let initialsData = (exactData, label) => (exactData) ? label + exactData : '' ;
+
     let htmlDetailsTemplate = "";
     htmlDetailsTemplate += `
     <article class="details-info">
         <div class="main-info">
             <div class="images">${showImg(specificCarDetails.images)}</div>
-            <img src="${specificCarDetails.images}" alt="${specificCarDetails.title}">
             <div class="initials-container">
                 <div class="brand-model">
                     <h2 class="car-brand details-header">${specificCarDetails.initials.brand}</h2>
                     <p class="car-model details-model">${specificCarDetails.title}</p>
-                    <span><p class="car-model details-variant">Variant: ${getData(this, specificCarDetails.initials.variant)}</p></span>
-                    <p class="car-model">Segment: ${specificCarDetails.initials.segment}</p>
-                    <p class="car-price details-price"><i class="fas fa-dollar-sign"></i> ${specificCarDetails.initials.price} DKK</p>
+                    <span><p class="car-model details-variant">${initialsData(specificCarDetails.initials.variant, 'Variant: ')}</p></span>
+                    <p class="car-model">${initialsData(specificCarDetails.initials.segment, 'Segment: ')}</p>
+                    <p class="car-price details-price">${initialsData(specificCarDetails.initials.price, '<i class="fas fa-dollar-sign"></i> ')} DKK</p>
                 </div>
-                <p class="car-model details-desc">DESC: ${getData(specificCarDetails.description)}</p>
+                <p class="car-model details-desc">${specificCarDetails.description}</p>
                 
             </div>
         </div>
         <div class="feature-container">
             <h3>Main features</h3>
             <div class="main-feature">
-                <p class="car-model">Range: ${getData(specificCarDetails.main.Rækkevide.data)} ${specificCarDetails.main.Rækkevide.after}</p>
-                <p class="car-model">Battery: ${specificCarDetails.main.Batteri.data} ${specificCarDetails.main.Batteri.after}</p>
-                <p class="car-model">Garanti: ${specificCarDetails.main.Garanti.data}</p>
-                <p class="car-model">0-100 km/h: </p>
-                <p class="car-model">Weight: ${specificCarDetails.main.Vægt.data} ${specificCarDetails.main.Vægt.after}</p>
+                <p class="car-model">${mainFeatureData(specificCarDetails.main.Rækkevide, 'Range: ')}</p>
+                ${(specificCarDetails.main.Batteri.data) ? `<p class="car-model">Battery: ${specificCarDetails.main.Batteri.data} ${specificCarDetails.main.Batteri.after}</p>` : ''}
+                <p class="car-model">${mainFeatureData(specificCarDetails.main.Garanti, 'Garanti: ')}</p>
+                <p class="car-model">${mainFeatureData(specificCarDetails.main["0 - 100 Km/t"], '0-100 km/h: ')}</p>
+                <p class="car-model">${mainFeatureData(specificCarDetails.main.Vægt, 'Weight: ')}</p>
             </div>
             <h3>Extra features</h3>
             <div class="extra-feature">
-                <p class="car-model">${specificCarDetails.extra["Træk på krog"]}</p>
-                <p class="car-model">${specificCarDetails.extra["Maksimum hastighed"]}</p>
-                <p class="car-model">${specificCarDetails.extra.Opladning}</p>
-                <p class="car-model">${specificCarDetails.extra["Opladning (hurtig)"]}</p>
+                <p class="car-model">${initialsData(specificCarDetails.extra["Træk på krog"], 'Træk på krog: ')}</p>
+                <p class="car-model">${initialsData(specificCarDetails.extra["Maksimum hastighed"], 'Maksimum hastighed: ')}</p>
+                <p class="car-model">${initialsData(specificCarDetails.extra.Opladning, 'Opladning: ')}</p>
+                <p class="car-model">${initialsData(specificCarDetails.extra["Opladning (hurtig)"], 'Opladning (hurtig): ')}</p>
                 
             </div>
         </div>
     </article>
     `
-    //<p class="car-model">: ${specificCarDetails.extra.Opladnin(hurtig)}</p>
     
     document.querySelector(".container").style.display = "none";
     document.querySelector(".main-header").innerHTML = "<h1 class='back' onclick='backToList()'><i class='fas fa-angle-left'></i> Go back</h1>";
     document.querySelector(".details").innerHTML = htmlDetailsTemplate;
+    document.querySelector(".details").style.display = "flex";
 }
-
-function getData(exactData){
-    console.log(event.target);
-    if (exactData){
-        return exactData;
-    }   else if(exactData === null){
-        
-        event.target.parentElement.remove();
-    }   else {
-        event.target.parentElement.remove();
-    }
-}
-
 
 function showImg(images){
      console.log(images);
@@ -124,16 +104,13 @@ function showImg(images){
             htmlImgTemplate += `
             <img src="${image}" alt="img">`  
         }
-        
         return htmlImgTemplate;
-         
      }  else {
          return "";
-     }
-     
-    
+     }  
 };
 
+//Back to homepage
 function backToList(){
     document.querySelector(".details").style.display = "none";
     document.querySelector(".container").style.display = "flex";
